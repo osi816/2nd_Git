35fcb5058c64e73f7b82a0ddda26dfda0831daa5
Lee Jeong Hun <wjdgns1155>
2018-04-19 19:25:54 +0900

    이정훈, mapper

--- mini_2nd/src/kr/co/bitnews/domain/NewsImg.java
+++ mini_2nd/src/kr/co/bitnews/domain/NewsImg.java
@@ -1,5 +1,28 @@
 package kr.co.bitnews.domain;
 
 public class NewsImg {
+	private int newsImgNo;
+	private int newsId;
+	private String imgPath;
+	
+	public int getNewsImgNo() {
+		return newsImgNo;
+	}
+	public void setNewsImgNo(int newsImgNo) {
+		this.newsImgNo = newsImgNo;
+	}
+	public int getNewsId() {
+		return newsId;
+	}
+	public void setNewsId(int newsId) {
+		this.newsId = newsId;
+	}
+	public String getImgPath() {
+		return imgPath;
+	}
+	public void setImgPath(String imgPath) {
+		this.imgPath = imgPath;
+	}
+	
 	
 }
---
+++ mini_2nd/src/kr/co/bitnews/mapper/NewsImgMapper.java
@@ -0,0 +1,5 @@
+package kr.co.bitnews.mapper;
+
+public interface NewsImgMapper {
+
+}
