57c66eca3e1ebb51f7e9663a65a21c78765dbd37
DESKTOP-U4MQOVV\da270 <da6225@gmail.com>
2018-04-19 19:18:45 +0900

    no message

--- mini_2nd/src/kr/co/bitnews/mapper/UserMapper.java
+++ mini_2nd/src/kr/co/bitnews/mapper/UserMapper.java
@@ -1,5 +1,9 @@
 package kr.co.bitnews.mapper;
 
-public interface UserMapper {
+import kr.co.bitnews.domain.User;
 
+public interface UserMapper {
+	String selectUserById(String id);
+	void insertUser(User user);
+	void updateUser(User user);
 }
