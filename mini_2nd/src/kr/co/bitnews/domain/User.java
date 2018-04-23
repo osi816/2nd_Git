deb06bc2e87bff1c38e3a8d2525ab7c1fa6a6338
DESKTOP-U4MQOVV\da270 <da6225@gmail.com>
2018-04-23 18:02:22 +0900

    no message

--- mini_2nd/src/kr/co/bitnews/domain/User.java
+++ mini_2nd/src/kr/co/bitnews/domain/User.java
@@ -1,13 +1,21 @@
 package kr.co.bitnews.domain;
 
+import java.util.Date;
+
 public class User {
 	private String userId;
-
     private String userPw;
-
     private String userName;
-
     private String userEmail;
+    private Date acceptTime;
+
+	public Date getAcceptTime() {
+		return acceptTime;
+	}
+
+	public void setAcceptTime(Date acceptTime) {
+		this.acceptTime = acceptTime;
+	}
 
 	public String getUserId() {
 		return userId;
