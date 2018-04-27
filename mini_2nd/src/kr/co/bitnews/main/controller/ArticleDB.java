2e62a8e2d53d4e8da6ce1f9771b4c6c2700f96e0
Lee Jeong Hun <wjdgns1155>
2018-04-27 16:35:02 +0900

    no message

---
+++ mini_2nd/src/kr/co/bitnews/main/controller/ArticleDB.java
@@ -0,0 +1,93 @@
+package kr.co.bitnews.main.controller;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import common.db.MyAppSqlConfig;
+import kr.co.bitnews.domain.Article;
+import kr.co.bitnews.mapper.ArticleMapper;
+
+@WebServlet("/articlecrawling")
+public class ArticleDB extends HttpServlet{
+
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArticleMapper mapper = MyAppSqlConfig.getSqlSession().getMapper(ArticleMapper.class);
+		System.setProperty("webdriver.chrome.driver", new File("C:\\java-lec\\2nd_git\\mini_2nd\\chrome\\chromedriver.exe").getAbsolutePath()); //크롬 드라이버 파일 경로설정
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.hellodd.com/?md=news");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); //응답시간 5초설정
+		String name = "";
+		List<WebElement> list = driver.findElements(By.cssSelector("#list_area > ul > li"));
+		List<Article> aList = new ArrayList<>();
+		for (WebElement e : list) {
+			Article a = new Article();
+			String[] noArr = e.findElement(By.cssSelector("div.article > h3 > a")).getAttribute("href").split("pid=");
+			Article no = mapper.selectArticleByNo(Integer.parseInt(noArr[1]));
+//			System.out.println("dbno :  scrapno : " + noArr[1]);
+			System.out.println("체크 시작!");
+			if (no == null) a.setArticleNo(Integer.parseInt(noArr[1]));
+			else continue;
+			System.out.println("널값 으로 else 건너뜀 DB에 데이터 저장 ");
+			a.setArticleType("hellodd");
+			a.setArticleTitle(e.findElement(By.tagName("h3")).getText());
+			a.setArticleUrl(e.findElement(By.cssSelector("div.article > h3 > a")).getAttribute("href"));
+			a.setArticleSummary(e.findElement(By.cssSelector("div.article > span > a")).getText());
+			try {
+				a.setArticleThumb(e.findElement(By.cssSelector("div.thumb > a > img")).getAttribute("src"));
+			}catch (NoSuchElementException e1) {
+				a.setArticleThumb(" ");
+			}
+			name = e.findElement(By.cssSelector("div.article > p")).getText();
+			String[] arr = name.split(" \\| ");
+			SimpleDateFormat sd = new SimpleDateFormat("yyyy.MM.dd");
+			if(arr.length == 2) {
+				a.setArticleReporter(arr[0]);
+				a.setArticleDate(arr[1]);
+			}else {
+				a.setArticleDate(arr[0]);
+				a.setArticleReporter("hellodd");
+			}
+//			mapper.insertArticle(a);
+//			System.out.println(a.toString());
+			aList.add(a);
+		}
+//		css 크롤링
+//		List<String> sList = new ArrayList<>();
+//		System.out.println("------------------------------");
+//		sList.add(driver.findElement(By.cssSelector("head > link:nth-child(9)")).getAttribute("href"));
+//		sList.add(driver.findElement(By.cssSelector("head > link:nth-child(10)")).getAttribute("href"));
+//		sList.add(driver.findElement(By.cssSelector("head > link:nth-child(11)")).getAttribute("href"));
+//		sList.add(driver.findElement(By.cssSelector("head > link:nth-child(12)")).getAttribute("href"));
+//		sList.add(driver.findElement(By.cssSelector("head > link:nth-child(13)")).getAttribute("href"));
+//		System.out.println(driver.findElement(By.cssSelector("head > link:nth-child(13)")).getAttribute("href"));
+//		System.out.println("------------------------------");
+		driver.close();
+//		for (Article a : aList) {
+//			System.out.println(a.toString());
+//			mapper.insertArticle(a);
+//		}
+		request.setAttribute("aList", aList);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/DB");
+		rd.forward(request, response);
+	}
+
+	
+}
